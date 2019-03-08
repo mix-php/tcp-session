99ca042033b50c6bbdf5e726467ca29cb4836c0c
Jian Liu <onanying@126.com>
2019-03-08 18:13:54 +0800

    上传

---
+++ src/HandlerInterface.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Mix\Tcp\Session;
+
+/**
+ * Interface HandlerInterface
+ * @package Mix\WebSocket\Session
+ * @author LIUJIAN <coder.keda@gmail.com>
+ */
+interface HandlerInterface
+{
+
+    /**
+     * 获取
+     * @param null $key
+     * @return mixed
+     */
+    public function get($key = null);
+
+    /**
+     * 设置
+     * @param $key
+     * @param $value
+     * @return bool
+     */
+    public function set($key, $value);
+
+    /**
+     * 删除
+     * @param $key
+     * @return bool
+     */
+    public function delete($key);
+
+    /**
+     * 清除
+     * @return bool
+     */
+    public function clear();
+
+    /**
+     * 判断是否存在
+     * @param $key
+     * @return bool
+     */
+    public function has($key);
+
+}
